dfd73d35fe1fe64d3a20b99b377f476c197671ee
Ahnoosh Winnie <ahnoosh18@gmail.com>
2020-10-30 20:16:05 +0800

    add link

--- vue.js/scripts/index.js
+++ vue.js/scripts/index.js
@@ -8,7 +8,7 @@ var app = new Vue({
 var app2 = new Vue({
     el: "#app-2",
     data: {
-        message: "You loaded this page on " + new DataCue().toLocalString(),
+        message: "You loaded this page on " + new Data().toLocalString(),
         linkURL: "https://www.google.com.my",
         linkText: "Go to Google Malaysia!"
     }
